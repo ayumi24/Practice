bc6e9c095e88661cd76455a2d779e7eb32721185
sasakiayumi <sasakiayumi@localhost>
2023-06-08 14:27:02 +0900

    拡張for文

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -3,19 +3,9 @@ package practice;
 public class Practice {
 
 	public static void main(String[] args) {
-		//switch文
-		int n = 1;
-		switch(n) {
-		 case 1 :
-			System.out.println("大吉です");
-			break;
-		 case 2 :
-			 System.out.println("吉です");
-			 break;
-		default :
-			System.out.println("凶です");
-			break;
+		String[] names = {"Kate" , "Jhon" , "Bob"};
+		for(String name: names) {
+			System.out.println("My name is" + name + ".");
 		}
 	}
-
 }
