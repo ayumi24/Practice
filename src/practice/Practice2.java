1702cb657f77522500999c9eb61c784352a8e7b3
sasakiayumi <sasakiayumi@localhost>
2023-06-15 22:28:42 +0900

    型変換について

--- src/practice/Practice2.java
+++ src/practice/Practice2.java
@@ -3,7 +3,13 @@ package practice;
 public class Practice2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 演算時の型変換
+		System.out.println(5/2);
+		// 片方がdouble型の場合
+		System.out.println(5.0/2);
+		
+		// 文字列を含む演算時の型変換
+		System.out.println("私の年齢は" + 23 + "歳です");
 
 	}
 
