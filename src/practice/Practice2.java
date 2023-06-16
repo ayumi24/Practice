44c628c268f1175a9f378bf701146b438d31a50c
sasakiayumi <sasakiayumi@localhost>
2023-06-16 17:52:32 +0900

    改行せずに文字列を表示する

--- src/practice/Practice2.java
+++ src/practice/Practice2.java
@@ -10,6 +10,12 @@ public class Practice2 {
 		
 		// 文字列を含む演算時の型変換
 		System.out.println("私の年齢は" + 23 + "歳です");
+		
+		// 改行せずに文字を表示する
+		String name = "歩実";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
 
 	}
 
