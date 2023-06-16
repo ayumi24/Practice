a8c0b3cb208572905c4a66487c3d5406ddf50ecc
sasakiayumi <sasakiayumi@localhost>
2023-06-16 18:06:52 +0900

    値の大きい方を代入する命令文

---
+++ src/practice/Practice3.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// 2つの値を比較して大きい方を代入
+		int a = 10;
+		int b = 12;
+		// 変数mを定義
+		int m = Math.max(a, b);
+		System.out.println("比較：" + a + "と" + b + "で大きいのは" + m);
+
+	}
+
+}
