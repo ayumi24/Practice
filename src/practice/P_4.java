1a5a0002719a3d165d6d90d63802e22ed253f389
sasakiayumi <sasakiayumi@localhost>
2023-06-17 22:46:21 +0900

    文字列を数字に変換

---
+++ src/practice/P_4.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class P_4 {
+
+	public static void main(String[] args) {
+		// 文字列を数字に変換する命令
+		String age = "23";
+		int n = Integer.parseInt(age);
+		System.out.println("来年私は" + (n + 1) + "歳です");
+
+	}
+
+}
