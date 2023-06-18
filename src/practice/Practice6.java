58d68e6df5739a82ff9790e408fcb9b772008f7e
sasakiayumi <sasakiayumi@localhost>
2023-06-18 13:31:56 +0900

    練習問題

---
+++ src/practice/Practice6.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice6 {
+
+	public static void main(String[] args) {
+		// 練習問題
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
+
+	}
+
+}
