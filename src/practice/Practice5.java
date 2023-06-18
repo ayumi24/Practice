c4a454b6e17fa1076efe4c32787c52a407853608
sasakiayumi <sasakiayumi@localhost>
2023-06-18 12:55:04 +0900

    キーボードから情報を受け取るコード

---
+++ src/practice/Practice5.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Practice5 {
+
+	public static void main(String[] args) {
+		// キーボードから文字列の入力を受け付ける
+		System.out.println("あなたの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("ようこそ" + name + "さん");
+		// キーボードから数字の入力を受け取る
+		System.out.println("年齢を教えてください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		System.out.println(input + "歳ですね");
+
+	}
+
+}
