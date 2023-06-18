b9cb724f92e05f6ffb4f86a911af96760fe7297f
sasakiayumi <sasakiayumi@localhost>
2023-06-18 12:30:19 +0900

    乱数を生み出す命令文

--- src/practice/P_4.java
+++ src/practice/P_4.java
@@ -7,6 +7,11 @@ public class P_4 {
 		String age = "23";
 		int n = Integer.parseInt(age);
 		System.out.println("来年私は" + (n + 1) + "歳です");
+		
+		// 乱数を生み出して代入
+		// 乱数は実行する度に90未満の数字からランダムに決まる
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたは" + r + "歳ですか？");
 
 	}
 
