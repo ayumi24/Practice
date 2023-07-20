c82b8ca52ba1c6e1fedd15a805674a6700b44e0d
sasakiayumi <sasakiayumi@localhost>
2023-07-20 14:28:51 +0900

    配列の要素を取得

---
+++ src/practice/Practice7.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Practice7 {
+
+	public static void main(String[] args) {
+		// 配列の要素数を取得する
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数：" + num);
+
+	}
+
+}
